3d7770e6fec24eb48c58cda8277a24aabee59dfd
r04944002 <r04944002@linux20.csie.ntu.edu.tw>
2016-01-01 16:20:30 +0800

    Plus +- sense

--- sentence2tag.py
+++ sentence2tag.py
@@ -18,7 +18,13 @@ sentence = raw_input("Input a sentence: ").decode(sys.stdin.encoding).encode('ut
 for tag, keywords in tags.items():
 	for keyword in keywords:
 		if keyword in sentence:
-			collected_tags.append(tag)
+			keyword_pos = sentence.index(keyword)
+			# if chinese 'no' in sentence no far before the keyword
+			if '\xe4\xb8\x8d' in sentence[keyword_pos-9:keyword_pos]:
+				sign = '-'
+			else:
+				sign = '+'
+			collected_tags.append({tag:sign})
 			break
 
 print collected_tags
