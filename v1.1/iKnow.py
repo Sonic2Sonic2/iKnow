55c168dc97efac5acdb130978b5fdde832f7d0a1
Yu-An Tai <amuro0@gmail.com>
2016-03-27 18:49:06 +0800

    Fix several bugs and use Yelp-python package
    Fix several bugs, output format, add more demo sentences.
    Replace getYelpResult function by Yelp-python package, and modify the
    relative parts.

--- v1.1/iKnow.py
+++ v1.1/iKnow.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import knowledge_manipulate as km
+import random
 
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
@@ -52,29 +53,40 @@ class iKnowMainWindow(Frame):   # Main UI interacting with user
     def knowledgeInit(self):
         print "Loading knowledge bases."
         self.shutdown_keyword = km.loadList("shutdown_keyword.txt")
-        self.randomkeyword = km.loadList("random_keyword.txt")
+        self.random_keyword = km.loadList("random_keyword.txt")
         return 0
 
     def pushButtonAndGetToWork(self):
+        print ""
         print "push button!"
         self.inputField.delete(0, 'end')    # clear inputField
         self.outputField.delete(0, 'end')   # clear outputField
 
-        #self.user_say = getSpeechThenToTextDev().decode("utf8")
-        self.user_say = getSpeechThenToText().decode("utf8")    # Get speech and also do speech to text
+        self.user_say = getSpeechThenToTextDev()
+        #self.user_say = getSpeechThenToText()       # Get speech and also do speech to text
         self.understanding()                        # Do natural language understanding
         self.readyForAction()                       # Ready for action
         return 0
 
     def understanding(self):
-        self.inputField.insert(0, self.user_say)    # Show what user say
+        if self.user_say == -1:
+            self.system_state = 90
 
-        for shutdown_term in self.shutdown_keyword:   # Shut down detected
-            if shutdown_term in self.user_say:
-                system_state = -1
-                return
+        else:
+            self.user_say = self.user_say.decode("utf8")
+            self.inputField.insert(0, self.user_say)    # Show what user say
 
-        self.system_state = 11   # Anyway, put to state 1-1 for testing
+            for shutdown_term in self.shutdown_keyword:   # Shut down detected
+                if shutdown_term in self.user_say:
+                    self.system_state = -1
+                    return
+
+            for random_term in self.random_keyword:
+                if random_term in self.user_say:
+                    self.system_state = 12
+                    return
+
+            self.system_state = 11   # Anyway, put to state 1-1 for testing
 
         return 0 
 
@@ -94,7 +106,11 @@ class iKnowMainWindow(Frame):   # Main UI interacting with user
             return 0
 
         elif self.system_state == 12: # 12:
-            pass
+            whatToEat()
+            return 0
+
+        elif self.system_state == 90:   # 90: speech input value error
+            print u"語音辨識錯誤，請重新輸入！"
             return 0
 
         else:
@@ -103,17 +119,23 @@ class iKnowMainWindow(Frame):   # Main UI interacting with user
 
 
 def getSpeechThenToTextDev(): # for silent testing while developing (kill this function when it's no use)
-    #dev_test_utt = "開發測試：請幫我找台大附近的義大利麵"  # normal test 
-    dev_test_utt = "開發測試：請幫我找一下台大後門附近的義大利麵"  # test loca_dict
-    #dev_test_utt = "開發測試：台大附近的義大利麵"  # test no location_front
-    #dev_test_utt = "開發測試：我想吃義大利麵"    # test no location
-    #dev_test_utt = "開發測試：台大附近的咖哩店"  # test no front
-    #dev_test_utt = "開發測試：台大附近義大利麵"  # test no front
-    #dev_test_utt = "開發測試：我想吃迴轉壽司" # test no-use category
-    #dev_test_utt = "開發測試：台大附近的牛肉麵"    # test no category
-    #dev_test_utt = "開發測試：台大附近奶凍捲"   # test no category and tag_front: CATASTROPHE
-    print dev_test_utt
+    #dev_test_utt = "請幫我找台大附近的義大利麵"  # normal test 
+    dev_test_utt = "請幫我找一下台大後門附近的義大利麵"  # test loca_dict
+    #dev_test_utt = "請幫我找一下台大附近的賣火鍋的"  # test 
+    #dev_test_utt = "台大附近的義大利麵"  # test no location_front
+    #dev_test_utt = "我想吃麵"  # test no location and category (use special term)
+    #dev_test_utt = "我想吃牛肉麵"  # test no location and category (use special term)
+    #dev_test_utt = "我想吃義大利麵"    # test no location
+    #dev_test_utt = "這裏有沒有優格"    # test no location and loca/cate keyword conflict
+    #dev_test_utt = "台大附近的咖哩店"  # test no front
+    #dev_test_utt = "台大附近義大利麵"  # test no front
+    #dev_test_utt = "我想吃迴轉壽司" # test no-use category
+    #dev_test_utt = "台大附近的牛肉麵"    # test no category
+    #dev_test_utt = "今天要吃什麼呢"   # test random
+    #dev_test_utt = "台大附近奶凍捲"   # test no category and tag_front: CATASTROPHE
+    print "測試：", dev_test_utt
     return dev_test_utt
+
 
 def getSpeechThenToText():    # see https://pypi.python.org/pypi/SpeechRecognition/ for detail
     r = sr.Recognizer()
@@ -142,39 +164,58 @@ def getSpeechThenToText():    # see https://pypi.python.org/pypi/SpeechRecogniti
                     c = ""
                     return c
             except sr.UnknownValueError:    # value error (not a recogizable utterance)
-                print("Oops! Didn't catch that")
+                #print("Oops! Didn't catch that")
+                return -1 # -1: speech input value error
             except sr.RequestError as e:    # service unavailable
                 print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
     except KeyboardInterrupt:
         pass
 
+
+def whatToEat():    ### DANGER, THIS PART SHOULD BE REWRITED ###
+    favorate_dictionary = km.loadList("favorate_dictionary.txt")
+
+    fav_list = []
+    for term in favorate_dictionary:
+        fav_list += [term]
+
+    print "不知道要吃什麼嗎？讓我來幫忙丟個飛鏢吧！！"
+    time.sleep(1.0)
+    print "登登！"
+    time.sleep(1.0)
+
+    print "===================="
+    for i in range(5):
+        choose_num = random.randint(0, len( fav_list )-1 )
+        print fav_list[ choose_num ]
+        fav_list.pop( choose_num )
+
+    print "===================="
+
+    return 0
+
 def getTag_Location(sentence):
-    collected_tags = getTag(sentence)[0]
+    tags = getTag(sentence) # tags[0]: category, [1]: term
+    cate = ','.join( tags[0] )
+    term = ','.join( tags[1] )
     geo = getLocation(sentence)
 
-    api_calls = []    
-    param = get_search_parameters(geo[0], geo[1], collected_tags[0])
-    api_calls.append( getYelpResults(param) )
-    jsonFromYelp = json.dumps(api_calls)
-    time.sleep(1.0)
-    restaurantData = json.loads(jsonFromYelp)
-    print "共找到", len(restaurantData[0]["businesses"]), "筆資料.\n"
+    param = getSearchParameters(cate, term)
+    result_from_yelp = getYelpResult(geo[:-1], param)
 
-    if len(restaurantData[0]["businesses"]) == 0:
+    if len(result_from_yelp.businesses) == 0:
         print "查無資料，請再次嚐試."
         return "（回傳句）查無資料", "（回傳地址）查無資料"
     else: 
         outputRestaurant = []
 
-        for oneData in restaurantData[0]["businesses"]:
-            print oneData["name"]
-            print oneData["location"]["address"]
-            #print oneData["categories"]
-            print oneData["distance"]
-            #outputRestaurant.append(oneData["name"])
+        for index in range( len(result_from_yelp.businesses) ):
+            print result_from_yelp.businesses[ index ].name
+            print result_from_yelp.businesses[ index ].location.address
+            print result_from_yelp.businesses[ index ].distance
 
-        responseSentence = ( '最高分回傳：'.decode('utf-8') + restaurantData[0]["businesses"][0]["name"] )
-        address = restaurantData[0]["businesses"][0]["location"]["address"]
+        responseSentence = ( '最高分回傳：'.decode('utf-8') + result_from_yelp.businesses[0].name )
+        address = result_from_yelp.businesses[0].location.address
 
         return responseSentence, address
 
@@ -182,6 +223,7 @@ def getTag_Location(sentence):
 def getTag(sentence_for_get_tag):
     category_dictionary = km.loadDictionary("categoryTW.txt")
     category_keyword_front = km.loadList("category_keyword_front.txt")
+    special_terms = km.loadList("special_terms.txt")
 
     tag_front = 0
     search_sen = ""
@@ -203,6 +245,14 @@ def getTag(sentence_for_get_tag):
                     find_term = [ term ]
                 elif len(find_term[0]) == len(term):     # same length term, check then join
                     find_category += [ category ]
+                    if term not in find_term: find_term += [ term ]
+
+    if len( find_term[0] ) == 0: # find no term, check special term ### DANGER, THIS PART SHOULD BE REWRITED ###
+        for term in special_terms:
+            if term in search_sen:
+                if len(find_term[0]) < len(term):
+                    find_term = [ term ]
+                elif len(find_term[0]) == len(term):
                     if term not in find_term: find_term += [ term ]
 
     print "category: ", find_category
@@ -228,28 +278,30 @@ def getLocation(sentence_for_get_location):  # here we use template sentence for
             location_back = int( sentence_for_get_location.find(term_back) )
 
     location = sentence_for_get_location[ location_front:location_back ]
-    print "location: ", location
 
-    if location_back == 0:  # Return with Error: Can not find a location, use default location: "NTU backdoor"
-        return [ 1, 25.0209219, 121.5403804 ]
+    if len(location) == 0:  # Return with Error: Can not find a location, use default location: "NTU backdoor"
+        print "location: 未偵測到地點，預設為台大後門 25.0209219, 121.5403804"
+        return [ 25.0209219, 121.5403804, 1 ]
     else:           # Return normally
+        print "location: ", location
         for coordinates_candidate in location_dictionary:   # try to find the location in location_dict
             for term in location_dictionary[ coordinates_candidate ]:
                 if location == term:
                     coordinates = [ float( coordinates_candidate.split(u',')[0] ),
-                                              float( coordinates_candidate.split(u',')[1] ), ]
+                                    float( coordinates_candidate.split(u',')[1] ), ]
                     print "coordinates: ", coordinates
                     return coordinates + [0]
 
+        coordinates = getGeocodeByGoogleMap( location.encode("utf8") ) # go GoogleMap for getting help
         print "coordinates: ", coordinates
-        return getGeocodeByGoogleMap( location.encode("utf8") ) + [0]  # go GoogleMap for getting help
+        return coordinates + [0]  # go GoogleMap for getting help
 
 
-def getGeocodeByGoogleMap(location_for_get_Geocode):    # notice that it can only eat str format, do encode("utf8") pls
+def getGeocodeByGoogleMap(location_for_get_geocode):    # notice that it can only eat str format, do encode("utf8") pls
     url = "https://maps.googleapis.com/maps/api/geocode/json?address="
     key = "&key=AIzaSyDU0SvmkoV9K_hm5xqDM39_-acX5HF7IW4"
 
-    googleapi_loca = url + "台灣+" + location_for_get_Geocode + key
+    googleapi_loca = url + "台灣+" + location_for_get_geocode + key
     rf = urllib.urlopen(googleapi_loca)
     json_from_GoogleMap = json.loads(rf.read())
     coordinates = [ json_from_GoogleMap["results"][0]["geometry"]["location"]["lat"],
@@ -258,49 +310,28 @@ def getGeocodeByGoogleMap(location_for_get_Geocode):    # notice that it can onl
     print 'Get GeoCode by Google Map! ' + str(coordinates[0]) + ' ' + str(coordinates[1])
     return coordinates
 
-#def getYelpResult(params):
-    #auth = Oauth1Authenticator(
-        #consumer_key = "zL6GUBjcMjK8xFsGhmirmg",
-        #consumer_secret = "LGix0hLov03PZ7z16svTs1dnMdc",
-        #token = "7xp93HFfj9gPddJud46SoFEphYObk9Oe",
-        #token_secret = "2U1XaBB3L8BRl4cbT1Ur9_h4bXM",
-    #)
-#    
-    #client = Client(auth)
 
-def getYelpResults(params): # btain these from Yelp's manage access page
-    consumer_key = "zL6GUBjcMjK8xFsGhmirmg"
-    consumer_secret = "LGix0hLov03PZ7z16svTs1dnMdc"
-    token = "7xp93HFfj9gPddJud46SoFEphYObk9Oe"
-    token_secret = "2U1XaBB3L8BRl4cbT1Ur9_h4bXM"
+def getYelpResult(coordinates, params): # obtain these from Yelp's manage access page
+    auth = Oauth1Authenticator(
+        consumer_key = "zL6GUBjcMjK8xFsGhmirmg",
+        consumer_secret = "LGix0hLov03PZ7z16svTs1dnMdc",
+        token = "7xp93HFfj9gPddJud46SoFEphYObk9Oe",
+        token_secret = "2U1XaBB3L8BRl4cbT1Ur9_h4bXM")
+    
+    client = Client(auth)
 
-    session = rauth.OAuth1Session(
-        consumer_key = consumer_key
-        ,consumer_secret = consumer_secret
-        ,access_token = token
-        ,access_token_secret = token_secret)
-
-    request = session.get("http://api.yelp.com/v2/search",params=params)
-
-    #Transforms the JSON API response into a Python dictionary
-    data = request.json()
-    session.close()
-
-    return data
+    return client.search_by_coordinates(coordinates[0], coordinates[1], **params)
 
 
-def get_search_parameters(inLat, inLong, inTerm):   # see the Yelp API for more details
+def getSearchParameters(inCate, inTerm):   # see the Yelp API for more details
     params = {}
-    params["term"] = inTerm
-    params["limit"] = 3
+    params["term"] = inTerm.encode("utf8")
+    params["limit"] = 5
     #params["offset"] = 100
-    #params["sort"] = 1
-    params["category_filter"] = "italian,japanese"
+    params["sort"] = 1
+    params["category_filter"] = inCate.encode("utf8")
     params["radius_filter"] = 1000
-    params["actionlinks"] = True
-
-    coordinateToYelp = str(inLat) + ',' + str(inLong)
-    params["ll"] = coordinateToYelp
+    #params["actionlinks"] = True
 
     params["cc"] = "TW"
 
@@ -308,6 +339,7 @@ def get_search_parameters(inLat, inLong, inTerm):   # see the Yelp API for more 
 
 
 if __name__ == '__main__':
+    print ""
     root = Tk()
     iKnow_Tk = iKnowMainWindow(master=root)
     iKnow_Tk.mainloop()
